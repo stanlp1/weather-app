f26c66f1b8a36b16618aef40dafbf8af293de77f
stanlp1 <stanleyp101@gmail.com>
2021-11-29 13:22:47 -0800

    Misc package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './Services/weatherApi'
+import { getCityWeather } from './Services/weatherApi';
+require('dotenv').config()
 
-
+console.log(getCityWeather("Rowland HEights"));
 ReactDOM.render(
   <React.StrictMode>
   </React.StrictMode>,
